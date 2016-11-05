f9c4fdba1d66f42faa6d24849f389562358d5083
Ricardo Lozano <lozanet@yahoo.com>
2016-11-04 18:42:20 -0700

    something

--- EvenTown BC/AccountCell.swift
+++ EvenTown BC/AccountCell.swift
@@ -29,7 +29,7 @@ class AccountCell: BaseCell {
     let profileNameLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "Ricardo Lozano"
+        label.text = "Ricardo E. Lozano"
         label.textColor = .darkGray
         return label
     }()
---
+++ EvenTown BC/SlideMenu.swift
@@ -0,0 +1,111 @@
+//
+//  SlideMenu.swift
+//  NetOps Status
+//
+//  Created by Ricardo Lozano on 11/4/16.
+//  Copyright © 2016 Social Reality Inc. All rights reserved.
+//
+
+import UIKit
+
+protocol SlideMenuDelegate {
+    func slideMenuItemSelectedAtIndex(_ index: Int32)
+}
+
+class SlideMenu: UIViewController, UITableViewDelegate {
+
+    var tblMenuOptions: UITableView!
+    var btnCloseMenuOverlay: UIButton!
+    var arrayMenuOptions = [Dictionary<String,String>]()
+    var btnMenu: UIButton!
+    
+    var delegate: SlideMenuDelegate?
+    
+    func updateArrayMenuOptions() {
+        arrayMenuOptions.append(["title":"Home", "icon":"HomeIcon"])
+        arrayMenuOptions.append(["title":"Play", "icon":"PlayIcon"])
+        
+        tblMenuOptions.reloadData()
+        
+    }
+    
+    func onCloseMenuClick(_ button: UIButton!) {
+        btnMenu.tag = 0
+        
+        if self.delegate != nil {
+            var index = Int32(button.tag)
+            if button == self.btnCloseMenuOverlay {
+                index = -1
+            }
+            delegate?.slideMenuItemSelectedAtIndex(index)
+        }
+        
+        UIView.animate(withDuration: 0.3, animations: { () -> Void in
+            self.view.frame = CGRect(x: -UIScreen.main.bounds.size.width, y: 0, width: UIScreen.main.bounds.size.width, height: UIScreen.main.bounds.size.height)
+            self.view.layoutIfNeeded()
+            self.view.backgroundColor = .clear
+        }, completion: { (finished) -> Void in
+            self.view.removeFromSuperview()
+            self.removeFromParentViewController()
+        })
+    }
+    
+    func addSlideMenuButton(){
+        let btnShowMenu = UIButton(type: UIButtonType.system)
+        btnShowMenu.setImage(self.defaultMenuImage(), for: UIControlState())
+        btnShowMenu.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        btnShowMenu.addTarget(self, action: #selector(onSlideMenuButtonPressed(_:)), for: UIControlEvents.touchUpInside)
+        let customBarItem = UIBarButtonItem(customView: btnShowMenu)
+        self.navigationItem.leftBarButtonItem = customBarItem;
+    }
+    
+    func defaultMenuImage() -> UIImage {
+        var defaultMenuImage = UIImage()
+        
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 30, height: 22), false, 0.0)
+        
+        UIColor.black.setFill()
+        UIBezierPath(rect: CGRect(x: 0, y: 3, width: 30, height: 1)).fill()
+        UIBezierPath(rect: CGRect(x: 0, y: 10, width: 30, height: 1)).fill()
+        UIBezierPath(rect: CGRect(x: 0, y: 17, width: 30, height: 1)).fill()
+        
+        UIColor.white.setFill()
+        UIBezierPath(rect: CGRect(x: 0, y: 4, width: 30, height: 1)).fill()
+        UIBezierPath(rect: CGRect(x: 0, y: 11,  width: 30, height: 1)).fill()
+        UIBezierPath(rect: CGRect(x: 0, y: 18, width: 30, height: 1)).fill()
+        
+        defaultMenuImage = UIGraphicsGetImageFromCurrentImageContext()!
+        
+        UIGraphicsEndImageContext()
+        
+        return defaultMenuImage;
+    }
+    
+    func onSlideMenuButtonPressed() {
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
